256f476be11f660d2a0a6450462ca5b8895e33cf
5610110629 <5610110629@psu.ac.th>
2020-05-08 23:20:02 +0700

    init commit

---
+++ Dice.java
@@ -0,0 +1,20 @@
+public class Dice{
+    private int face;
+    public Dice(){
+        face = 1;
+    }
+
+    public void roll(){
+        face = ((int)(Math.random() * 6) + 1);
+    }
+
+    public int getFace(){
+        return face;
+    }
+
+    /*public String toString(){
+        String display;
+        display = "face" + " " + "=" + " " + face;
+        return display;
+    }*/
+}
