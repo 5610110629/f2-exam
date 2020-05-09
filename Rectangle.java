79da9460a9cffa4a1df31fc2cf16528e4994087f
5610110629 <5610110629@psu.ac.th>
2020-05-09 11:30:34 +0700

    commit2

---
+++ Rectangle.java
@@ -0,0 +1,30 @@
+public class Rectangle extends Shape {	//concrete class
+	private double width;
+	private double height;
+
+	public Rectangle(String name, double width, double height){
+		super(name);
+		this.width = width;
+		this.height = height;
+	}
+
+	public double getWidth(){
+		return width;
+	}
+
+	public void setWidth(double width){
+		this.width = width;
+	}
+
+	public double getHeight(){
+		return height;
+	}
+
+	public void setHeight(double height){
+		this.height = height;
+	}
+
+	public double getArea(){
+		return width * height;
+	}
+}
